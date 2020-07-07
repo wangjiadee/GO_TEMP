0085039d0a15cdff9832d21bfb6cc6ebbff1dfe7
ralph <515002119@qq.com>
2020-07-07 21:04:07 +0800

    [go][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{if else 案例}
    风险及影响[快/稳/省/功能]：{无}
    测试建议：{无需测试}
    跨组依赖(提交链接)：{无}
    移植适用范围：{无}"

--- goele/06-if语句/4.if..else结构案例.go
+++ goele/06-if语句/4.if..else结构案例.go
@@ -2,28 +2,21 @@ package main
 
 import "fmt"
 
-func main()  {
-	 var money float64
-	 fmt.Println("请输入钱数")
-	 fmt.Scan(&money)
-	 // (2) 对钱数进行判断
-	 if money > 2{
-		 // (3) 如果钱数大于2元，条件成立，判断座位数。
-		 // (3.1) 要求用户输入座位数.
-		 var count int
-		 fmt.Println("请输入座位数：")
-		 fmt.Scan(&count)
-		 // (3.2) 对输入的座位数进行判断。
-		 if count > 0{
-		 	fmt.Println("你可以坐下！！")
-		 } else {
-		 	fmt.Println("你只能站着！！")
-		 }
-	 } else {
-		 // (4) 如果钱数不满足，只能输出”不能上车”
-		 fmt.Println("你不能上车！！")
-	 }
-
-
-
+// if else 结构案例
+func main() {
+	var money float64
+	fmt.Println("Pelase input money:")
+	fmt.Scan(&money)
+	if money >= 2 {
+		var count int
+		fmt.Println("Pelase input count imformation:")
+		fmt.Scan(&count)
+		if count > 0 {
+			fmt.Println("down!")
+		} else {
+			fmt.Println("up!")
+		}
+	} else {
+		fmt.Println("gun !")
+	}
 }
