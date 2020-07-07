26cadf0fd72f1542250ddec585aef492276fad6c
ralph <515002119@qq.com>
2020-07-07 21:16:28 +0800

    [go][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{if else if 语法}
    风险及影响[快/稳/省/功能]：{无}
    测试建议：{无需测试}
    跨组依赖(提交链接)：{无}
    移植适用范围：{无}

--- goele/06-if语句/5.if..else if结构.go
+++ goele/06-if语句/5.if..else if结构.go
@@ -2,22 +2,39 @@ package main
 
 import "fmt"
 
-func main()  {
-   // 1: 接收考试成绩
-   var score float64
-   fmt.Println("请输入考试成绩:")
-   fmt.Scan(&score)
-   // 2: 对考试成绩进行判断
-   if score >= 90 {
-   		 fmt.Println("A")
-   } else if score >= 80 {
-   		fmt.Println("B")
-   } else if score >= 70 {
-   		fmt.Println("C")
-   } else if score >= 60 {
-   		fmt.Println("D")
-   } else {
-   		fmt.Println("E")
-   }
-   // 3: 打印相应的信息
+// if else if 基础语法结构
+func main() {
+	var score float64
+	fmt.Println("input u score:")
+	fmt.Scan(&score)
+	if score >= 90 {
+		fmt.Println("A")
+	} else if score >= 80 {
+		fmt.Println("B")
+	} else if score >= 70 {
+		fmt.Println("C")
+	} else if score >= 60 {
+		fmt.Println("D")
+	} else {
+		fmt.Println("E")
+	}
+}
+
+// if else if 案例
+func main() {
+	var username string
+	var password string
+	fmt.Println("Pelase input u username:")
+	fmt.Scan(&username)
+	fmt.Println("Pelase input u Password:")
+	fmt.Scan(&password)
+	if username == "mdzz" && password == "sb" {
+		fmt.Println("loading success!")
+	} else if username == "mdzz" {
+		fmt.Println("password Error!")
+	} else if password == "sd" {
+		fmt.Println("Username Error!")
+	} else {
+		fmt.Println("password or Username Error!")
+	}
 }
