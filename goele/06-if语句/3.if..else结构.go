1d073a530f2447d880aff9cd6a91264d522814c4
ralph <515002119@qq.com>
2020-07-07 20:57:07 +0800

    [go][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{if else 語句}
    风险及影响[快/稳/省/功能]：{无}
    测试建议：{无需测试}
    跨组依赖(提交链接)：{无}
    移植适用范围：{无}"

--- goele/06-if语句/3.if..else结构.go
+++ goele/06-if语句/3.if..else结构.go
@@ -2,28 +2,24 @@ package main
 
 import "fmt"
 
-func main()  {
-	/*
-	var age int = 15
+// if else 大致结构
+func main() {
+	var age int
 	if age >= 18 {
-		fmt.Println("恭喜你已经成年了!!")
-	} else { // else 书写位置不要写错
-		fmt.Println("你没有成年!!")
+		fmt.Println("u can see yellow video!")
+	} else {
+		fmt.Println("studying hard now!")
 	}
-	*/
-	//小王的考试成绩大于等于90分,那么爸爸奖励他100元钱,否则
+}
 
-	//的话,爸爸就让小王跪方便面。
-
-	// 1:定义一个变量存储小王的考试成绩。
+// 自动交互加if else 语句
+func main() {
 	var score int
-	// 2: 录入考试成绩
-	fmt.Println("请输入考试成绩:")
+	fmt.Println("pelase input fucking score:")
 	fmt.Scan(&score)
-	// 3: 对考试成绩进行判断，然后打印出不同的结果。
-	if score >= 90{
-		fmt.Println("奖励100元")
-	}else{
-		fmt.Println("跪方便面")
+	if score >= 90 {
+		fmt.Println("give u ￥100")
+	} else {
+		fmt.Println("fucking u !")
 	}
 }
