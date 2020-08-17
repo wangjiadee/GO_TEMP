363379549f716b4267f6ce4385665e840266195c
wangjiade <wangjiade@oppo.com>
2020-08-17 17:23:22 +0800

    start new go

---
+++ Ralph.org/listen1/variable/main.go
@@ -0,0 +1,21 @@
+/*
+ * @Author: Ralph
+ * @Date: 2020-08-17 16:24:23
+ * @LastEditTime: 2020-08-17 16:26:34
+ * @LastEditors: Please set LastEditors
+ * @Description: GO
+ * @FilePath: \go\src\github.com\Ralph.org\listen1\variable\main.go
+ */
+package main
+
+func main() {
+	var a int
+	var b bool
+	var s string
+	//  两种定义方式
+	var (
+		a1 int
+		b1 bool
+		s1 string
+	)
+}
