da3b2fcaf33d8fe5d08a561464217a8827b63c22
wangjiade <wangjiade@oppo.com>
2020-08-26 15:06:41 +0800

    newgo

---
+++ Ralph.org/element/go_package_method/cala_run/main.go
@@ -0,0 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-26 15:00:20
+ * @LastEditTime: 2020-08-26 15:00:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \newgo\Ralph.org\element\go_package_method\cala_run\main.go
+ */
+package main
+
+import (
+	"fmt"
+
+	"github.com/Ralph.org/element/go_package_method/cala"
+)
+
+func main() {
+	var sum = cala.Add(2, 3)
+	fmt.Println(sum)
+}
