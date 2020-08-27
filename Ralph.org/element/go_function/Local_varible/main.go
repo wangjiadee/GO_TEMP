2fd9f5c7dd224318c827fa2b24510ad052547cfb
wangjiade <wangjiade@oppo.com>
2020-08-27 17:06:08 +0800

    [go][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{go 函数相关}
    风险及影响[快/稳/省/功能]：{无}
    测试建议：{无需测试}
    跨组依赖(提交链接)：{无}
    移植适用范围：{无}

---
+++ Ralph.org/element/go_function/Local_varible/main.go
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-27 15:12:56
+ * @LastEditTime: 2020-08-27 15:12:59
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \newgo\Ralph.org\element\go_function\Global_var\main.go
+ */
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	TestA()
+	fmt.Println(a)
+}
+func TestA() {
+	a := 6
+	a++
+
+}
