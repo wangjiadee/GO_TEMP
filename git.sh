92b6006991ac95d7f8e6c5c293329c0b98001b84
wangjiade <515002119@qq.com>
2020-09-07 14:17:11 +0800

    [go][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{提交脚本修改}
    风险及影响[快/稳/省/功能]：{无}
    测试建议：{无需测试}
    跨组依赖(提交链接)：{无}
    移植适用范围：{无}

--- git.sh
+++ git.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+# @Ralph|2020.09.04
+
 
 set -x
-
+GITFILE = "GIT.txt"
 GOPATH="/go/src/github.com/go"
-mkdir ${GOPATH}/GIT.txt
-export Save_Git_Branch_Path="${GOPATH}/GIT.txt"
+mkdir ${GOPATH}/${GITFILE}
+export Save_Git_Branch_Path="${GOPATH}/${GITFILE}"
 Git_Branch = `awk -F " " '{print $2}' "${Save_Git_Branch_Path}" `
 
-git branch > $Save_Git_Branch_Path
+git branch > ${Save_Git_Branch_Path}
 
 git add . 
 
@@ -20,5 +22,5 @@ git commit -m "[go][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集
 
 git push
 
-rm -rf ${GOPATH}/GIT.txt
+rm -rf ${Save_Git_Branch_Path}
 
