7adc3860d359223de1d8b74bbd7e7e5a788298a9
wangjiade <515002119@qq.com>
2020-09-08 15:19:06 +0800

    [go][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{change git script}

--- git.sh
+++ git.sh
@@ -9,7 +9,9 @@ touch ${GOPATH}/GIT.txt
 export Save_Git_Branch_Path="${GOPATH}/GIT.txt"
 git branch > ${Save_Git_Branch_Path}
 Git_Branch = `awk -F " " '{print $2}' "${Save_Git_Branch_Path}" `
+echo "Get git brach finish....."
 
+echo "Push code ....."
 git add . 
 
 git commit -m "[go][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{$1}"
